407a75825eb412a7d6a8e411d98af238579da34c
Jongseung (John) Lim <penandlim@gmail.com>
2015-04-17 17:24:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ window.onload = function () {
         $('#test2').html('');
         $('#test3').html('');
         $('#test4').html('YES!');
-        $("#test4").css({'transition': 'all 1s ease-in-out', 'transform': 'scale(2)'});
+        $("#test4").css({'transition': 'all 1s ease-in-out', 'transform': 'scale(5)'});
         for (var i = 0; i < 9; i++) {
             periods[i].html('100.000%');
             periods[i].attr('class', 'green-text text-darken-2');
